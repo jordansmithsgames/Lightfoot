99ceb2d0f4bbcc6d1d45229daeb4337d430a2bff
Linkraft <36479127+Linkraft@users.noreply.github.com>
2020-04-05 12:49:46 -0400

    Dropdown platforms added

---
+++ Lightfoot/Assets/Scripts/Dropdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropdown : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    void Start()
+    {
+        effector = gameObject.GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Crouch"))
+        {
+            Flip();
+            Invoke("Flip", 0.5f);
+        }
+    }
+
+    private void Flip()
+    {
+        if (effector.rotationalOffset == 180f) effector.rotationalOffset = 0f;
+        else effector.rotationalOffset = 180f;
+    }
+}
+
