22563f35b0833371ee410b955f630be98bec78f8
Linkraft <36479127+Linkraft@users.noreply.github.com>
2020-04-06 21:01:02 -0400

    Started on monster fists

---
+++ Lightfoot/Assets/Scripts/FistSlam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class FistSlam : MonoBehaviour
+{
+    public float speed = 10f;
+    public float delta = 5f;
+    private Vector3 finalPosition;
+
+    // Start is called at the beginning of the first frame
+    void Start() {
+        finalPosition = transform.position;
+        transform.position = new Vector3(transform.position.x, transform.position.y + delta, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = Vector3.MoveTowards(transform.position, finalPosition, speed * Time.deltaTime);
+    }
+}
