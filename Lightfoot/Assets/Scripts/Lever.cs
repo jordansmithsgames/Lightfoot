a199ea356fc85d08605ee480cbd30a5fc1103566
Linkraft <36479127+Linkraft@users.noreply.github.com>
2020-04-15 11:15:39 -0400

    Woooo

--- Lightfoot/Assets/Scripts/Lever.cs
+++ Lightfoot/Assets/Scripts/Lever.cs
@@ -6,40 +6,27 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class Lever : MonoBehaviour
 {
     public Animator animator;
-    public Light2D lightbulb;
     public bool activated;
-
-    private float intensity;
-    private bool triggering = false;
+    private bool triggering;
 
     void Start() {
-        intensity = lightbulb.intensity;
         animator.SetBool("Activated", activated);
-        //UpdateLight();
+        triggering = false;
     }
 
     void Update() {
-        if (triggering && Input.GetButtonDown("Interact")) PullLever();
+        if (triggering && Input.GetButtonDown("Interact"))
+        {
+            animator.SetBool("Activated", !activated);
+            activated = !activated;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
-        Debug.Log(collision.gameObject.name);
         triggering = true;
     }
 
     void OnTriggerExit2D(Collider2D collision) {
         triggering = false;
-    }
-
-    void PullLever() {
-        animator.SetBool("Activated", !activated);
-       
-        UpdateLight();
-        activated = !activated;
-    }
-
-    void UpdateLight() {
-        if (activated) lightbulb.intensity = intensity;
-        else lightbulb.intensity = 0;
     }
 }
---
+++ Lightfoot/Assets/Scripts/Light.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Light : MonoBehaviour
+{
+    public Lever lever;
+    public bool lit = true;
+    private Light2D light;
+    private float initIntensity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = gameObject.GetComponent<Light2D>();
+        initIntensity = light.intensity;
+        if (!lit) light.intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lever.activated) lit = true;
+        else if (!lever.activated) lit = false;
+        if (lit) light.intensity = initIntensity;
+        else light.intensity = 0;
+    }
+}
