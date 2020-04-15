ab9414be5c909eb4ea0386272fc16187b8de7fcb
Julian <frardvark@gmail.com>
2020-04-14 22:35:10 -0400

    Pause Menu

---
+++ Lightfoot/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    PauseManager pauseManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseManager = GameObject.Find("Player").GetComponent<PauseManager>();
+    }
+
+    // Update is called once per frame
+    public void exit()
+    {
+        pauseManager.exitGame();
+    }
+}
---
+++ Lightfoot/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseMenu = GameObject.FindGameObjectWithTag("Pause");
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                showPaused();
+            }
+            else
+            {
+                hidePaused();
+            }
+        }
+    }
+
+    public void showPaused()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void hidePaused()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
